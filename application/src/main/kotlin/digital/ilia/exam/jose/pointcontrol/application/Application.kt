3aaa87a59fd923b580258663d66c3bf3b3a50f99
José Nicodemos Maia Neto <jose+github.com@nicomaia.com.br>
2024-08-16 10:38:24 -0300

    feat: added missing files

---
+++ application/src/main/kotlin/digital/ilia/exam/jose/pointcontrol/application/Application.kt
@@ -0,0 +1,11 @@
+package digital.ilia.exam.jose.pointcontrol.application
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+	runApplication<Application>(*args)
+}
---
+++ application/src/test/kotlin/digital/ilia/exam/jose/pointcontrol/application/PointcontrolApplicationTests.kt
@@ -0,0 +1,13 @@
+package digital.ilia.exam.jose.pointcontrol
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PointcontrolApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
